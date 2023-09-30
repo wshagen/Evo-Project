ff1bd14c5cfd3f4ef4a8c700789b6eca63f5b44c
Will Hagen <wshagen3@gmail.com>
2023-09-30 17:05:09 -0500

    updated JS/SCSS

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1,8 +1,9 @@
 
 
   
-
+const header = document.querySelector('header');
 const body = document.querySelector('body');
+const para = document.querySelectorAll('P');
 const modeToggle = document.getElementById('mode-toggle');
 const modeStatus = document.querySelector('.mode-status');
 
@@ -15,6 +16,12 @@ function toggleMode() {
     'Dark Mode' 
     : "Light Mode"
   body.classList.toggle('gradient');
+  
+  for(const i of para) {
+    i.classList.toggle('paradark');
+  }
+  header.classList.toggle('header');
+  header.classList.toggle('darkheader');
   modeStatus.innerText = "Currently in " + modeMessage;
 }
 
